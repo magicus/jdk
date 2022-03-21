ba0de4909c4dd5bc93e01bf5db384f1d9499f62d
Magnus Ihse Bursie <mag@icus.se>
2022-03-21 23:33:44 +0100

    FIx merge error properly

--- src/java.base/share/tools/org/openjdk/buildtools/cldrconverter/DayPeriodRuleParseHandler.java
+++ src/java.base/share/tools/org/openjdk/buildtools/cldrconverter/DayPeriodRuleParseHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
  * questions.
  */
 
-package build.tools.cldrconverter;
+package org.openjdk.buildtools.cldrconverter;
 
 import java.io.File;
 import java.io.IOException;
