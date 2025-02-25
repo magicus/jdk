f7ce406e3dd3f6988a3a14c8d229d395c07deff6
Magnus Ihse Bursie <mag@icus.se>
2025-02-25 17:14:10 +0100

    Prepare main launcher for STATIC_BUILD define

--- src/java.base/share/native/launcher/defines.h
+++ src/java.base/share/native/launcher/defines.h
@@ -26,50 +26,5 @@
 #ifndef _DEFINES_H
 #define _DEFINES_H
 
-#include "java.h"
 
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
-
-/*
- * This file contains commonly defined constants used only by main.c
- * and should not be included by another file.
- */
-#ifndef VERSION_STRING
-/* make sure the compilation fails */
-#error "VERSION_STRING must be defined"
-#endif
-
-/* Unused, but retained for JLI_Launch compatibility*/
-#define DOT_VERSION "0.0"
-
-#ifdef JAVA_ARGS
-#ifdef PROGNAME
-static const char* const_progname = PROGNAME;
-#else
-static char* const_progname = NULL;
-#endif
-static const char* const_jargs[] = JAVA_ARGS;
-#else  /* !JAVA_ARGS */
-static const char* const_progname = "java";
-static const char** const_jargs = NULL;
-#endif /* JAVA_ARGS */
-
-#ifdef LAUNCHER_NAME
-static const char* const_launcher = LAUNCHER_NAME;
-#else  /* LAUNCHER_NAME */
-static char* const_launcher = NULL;
-#endif /* LAUNCHER_NAME */
-
-#ifdef EXPAND_CLASSPATH_WILDCARDS
-static const jboolean const_cpwildcard = JNI_TRUE;
-#else
-static const jboolean const_cpwildcard = JNI_FALSE;
-#endif /* EXPAND_CLASSPATH_WILDCARDS */
-
-#ifdef ENABLE_ARG_FILES
-static const jboolean const_disable_argfile = JNI_FALSE;
-#else
-static const jboolean const_disable_argfile = JNI_TRUE;
-#endif
 #endif /*_DEFINES_H */
