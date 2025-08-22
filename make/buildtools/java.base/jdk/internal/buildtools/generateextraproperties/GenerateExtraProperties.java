11205880e6981448f186efa7367b1ceac51c33a2
Magnus Ihse Bursie <mag@icus.se>
2025-08-22 14:38:48 +0200

    Clean up code around some moved tools

--- make/buildtools/java.base/jdk/internal/buildtools/generateextraproperties/GenerateExtraProperties.java
+++ make/buildtools/java.base/jdk/internal/buildtools/generateextraproperties/GenerateExtraProperties.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package build.tools.generateextraproperties;
+package jdk.internal.buildtools.generateextraproperties;
 
 import java.io.IOException;
 import java.nio.file.Files;
