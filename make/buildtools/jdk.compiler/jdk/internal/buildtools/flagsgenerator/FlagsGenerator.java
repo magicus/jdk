7428c92a60912c0a7eacc28f10cf4306850d19ce
Magnus Ihse Bursie <mag@icus.se>
2025-08-22 14:45:50 +0200

    Handle flagsgenerator

--- make/buildtools/jdk.compiler/jdk/internal/buildtools/flagsgenerator/FlagsGenerator.java
+++ make/buildtools/jdk.compiler/jdk/internal/buildtools/flagsgenerator/FlagsGenerator.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package flagsgenerator;
+package jdk.internal.buildtools.flagsgenerator;
 
 import com.sun.source.tree.CompilationUnitTree;
 import com.sun.source.util.JavacTask;
